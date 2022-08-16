01cbb6d513cf80967b9e588440e10f75ebbc1d55
jknithin36 <89259785+jknithin36@users.noreply.github.com>
2022-08-16 19:43:41 +0530

    Add files via upload

---
+++ FunctionBasic.java
@@ -0,0 +1,16 @@
+public class FunctionBasic {
+  public static void main(String[]args){
+       PrintLines();
+       PrintLines();
+       PrintLines();
+       PrintLines();
+       PrintLines();
+       PrintLines();
+  }
+  public static void PrintLines(){
+System.out.println("I will not copy and paste code.");
+System.out.println("I will use Camel Case when writing class names.");
+System.out.println("I will use lower Camel Case when writing function names.");
+System.out.println("I will use lower Camel Case when writing variables names.\n");
+  }
+}
